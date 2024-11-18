4638513550dad29fcb6f6f6aa5cf8a84b5bc074b
Gerardo Rodriguez <55035792+rodriguez-g@users.noreply.github.com>
2024-11-18 19:42:26 +0000

    removed the other summary

--- src/WorkshopDemo/WeatherForecast/WeatherForecastController.cs
+++ src/WorkshopDemo/WeatherForecast/WeatherForecastController.cs
@@ -12,7 +12,8 @@ public class WeatherForecastController(ILogger<WeatherForecastController> logger
         logger.LogInformation("Get WeatherForecast called");
         var summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            // "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing"
         };
         var forecasts =  Enumerable.Range(1, 5).Select(index =>
                 new WeatherForecast
